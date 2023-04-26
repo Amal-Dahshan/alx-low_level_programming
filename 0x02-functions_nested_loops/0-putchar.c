51b455eaba28d90076833b5d1575b96113a488fd
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-04-25 19:39:02 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-/* main function - print a string */
+#include "main.h"
 
-int main(void){
-	char str[] = {'S', 't', 'r', 'i', 'n', 'g', '\0'};
-	int i = 0;
-    
-    // looping till the null character is encountered
-    while(str[i] != '\0')
-    {
-        putchar("%c", str[i]);
-        i++;
-    }
-	return 0;
-}
+/**
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
+
+
+int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
