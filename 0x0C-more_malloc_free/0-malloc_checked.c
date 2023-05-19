220018da966aa97b72d72ea452e4273072f77330
Amal-Dahshan <amaldahshan11710@gmail.com>
2023-05-18 17:00:11 -0700

    new

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,10 +7,11 @@
  *
  * Return: a pointer to the allocated memory
  */
-void *malloc_checked(unsigned int b) {
+void *malloc_checked(unsigned int b)
+{
 	void *ptr = malloc(b);
-	if (ptr == NULL) {
+
+	if (ptr == NULL)
 		exit(98);
-	}
-	return ptr;
+	return (ptr);
 }
